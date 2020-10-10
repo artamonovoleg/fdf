727186dd87c0d5db3578539ce1e3e9f2a5c72d07
artamonovoleg <artamonovav2003@gmail.com>
2020-10-10 13:32:34 +0300

    0.1 working version

--- src/Gui.cpp
+++ src/Gui.cpp
@@ -4,7 +4,7 @@
 
 #include "Gui.hpp"
 
-Gui::Gui(sdl::Window &window) : font("Roboto-Medium.ttf", 24), text(font, "How to use: \nMoveg: A, S, W, D \nRotate: I, J, K, L \nChange color: c\n ", SDL_Color {255, 255, 255})
+Gui::Gui(sdl::Window &window) : font("Roboto-Medium.ttf", 24), text(font, "How to use: \nMove: A, S, W, D \nRotate: I, J, K, L \nChange color: c\n ", SDL_Color {255, 255, 255})
 {
     int window_width = window.getWidth();
 
